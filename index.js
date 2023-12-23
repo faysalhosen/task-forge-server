6224787e2acb11bc3a2bd755259f4f2c3a1f2966
faysalhosen <faysanhosen7025@gmail.com>
2023-12-23 12:03:59 +0600

    overall setup

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5001;
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wzxk65v.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+   
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("TaskForge server is running");
+});
+
+app.listen(port, () => {
+  console.log(`TaskForge server is running on port:${port}`);
+});
